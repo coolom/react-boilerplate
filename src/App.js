1ac588dba17d579cd258c395bf5f5e34f0bfc6c7
onkar kulkarni <maconkar@onkars-MacBook-Pro.local>
2019-12-21 23:11:21 -0500

    added webpack configs and style loaders

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import { hot } from "react-hot-loader";
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!...</h1>
+        <h2 className={this.state.count > 7 ? "warning" : null}>
+          Count: {this.state.count}
+        </h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}>
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
